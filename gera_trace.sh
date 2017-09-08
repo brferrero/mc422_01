da3a5af8de122b6501dda67d7954a14a79f0f105
brferrero <brferrero@gmail.com>
2017-09-08 14:13:17 -0300

    gerador de arquivos teste

---
+++ gera_trace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# gera arquivos traces para o simulador de processos\
+# ./gera_trace.sh 5
+#
+
+NPROCESSOS=${1:-10}
+
+d0=1.0
+dt=3.0
+df=4.0
+
+i=0
+echo ${d0} ${dt} ${df} processo${i} >trace_${NPROCESSOS}.txt
+
+for i in `seq 1 ${NPROCESSOS}`; do
+    delta=$((1 + RANDOM % 10))
+    d0=`echo ${d0} + 0.${delta} | bc` 
+    dt=$((0 + RANDOM % 6))
+    delta=$((1 + RANDOM % 9))
+    df=`echo ${d0} + ${dt}.${delta} +${df} | bc` 
+    
+    echo ${d0} ${dt}.${delta} $df processo${i} >>trace_${NPROCESSOS}.txt
+done
